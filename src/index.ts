866808a68caba5d26d5fae82223f74a05b763452
Dong Zhou <dozhou@rubiconproject.com>
2026-01-15 20:12:11 -0800

    Delete index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export interface HelloOptions {
-  name?: string;
-}
-
-export function hello(options: HelloOptions = {}): string {
-  const name = options.name ?? "world";
-  return `Hello, ${name}!`;
-}
-
